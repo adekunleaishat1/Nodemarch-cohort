5ce7ef13a2f1cf81be7bb4f03634d599bdf805f6
adekunleaishat1 <aishatadekunle877@gmail.com>
2025-03-14 11:08:12 +0100

    mongoose schema and model

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express")
 const app = express()
 const ejs = require("ejs")
+const mongoose = require("mongoose")
 
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended:true}))
 
+ // CRUD CREATE READ UPDATE DELETE
+ const userschema = new mongoose.Schema({
+     firstname:{type:String, required:true},
+     lastname:{type:String, required:true},
+     email:{type:String,  required:true, unique:true},
+     password:{type:String,  required:true}
+  })
+
+ const usermodel = mongoose.model("user_collection", userschema)
 
 let userarray = []
 let todoarray = []
@@ -64,42 +74,76 @@ app.post("/todo/update/:index",(req, res)=>{
     
 })
 
-app.post("/user/register",(req, res)=>{
- console.log(req.body);
- const {firstname, lastname, email, password} = req.body
-   if (!firstname || !lastname || !email || !password) {
-    message = "input field cannot be empty"
-      // console.log();
-      res.redirect("/")
-   }else{
-    userarray.push(req.body)
-     res.redirect("/login")
-   }
-   
+app.post("/user/register",async (req, res)=>{
+try {
+  console.log(req.body);
+  const {firstname, lastname, email, password} = req.body
+    if (!firstname || !lastname || !email || !password) {
+     message = "input field cannot be empty"
+       // console.log();
+       res.redirect("/")
+    }else{
+     const creteduser =  await usermodel.create(req.body)
+     console.log(creteduser);
+     
+      res.redirect("/login")
+    }
+} catch (error) {
+  console.log(error);
+  if (error.message.includes("E11000 duplicate key error")) {
+    message = "user already exist"
+    res.redirect("/")
+  }
+  
+  
+} 
 })
 
-app.post("/user/login",(req, res)=>{
-  console.log(req.body);
+app.post("/user/login", async(req, res)=>{
+  try {
+    console.log(req.body);
   const {email, password} = req.body
   if(!email || !password){
      console.log("input field cannot be empty");
      
   }else{
-   const existuser = userarray.find((user)=> user.email == email)
+   const existuser =  await  usermodel.findOne({email})
    console.log(existuser);
-   if (existuser && existuser.password == password) {
-     console.log("login successful");
-     res.redirect("/login")
+   if (existuser && existuser.password === password) {
+    console.log("login successful");
+       res.redirect("/login")
    }else{
     console.log("User not found");
-    res.redirect("/login")
+      res.redirect("/login")
    }
-   
-
+  
+  }
+  } catch (error) {
+    console.log(error);
+    
   }
   
 })
 
+
+
+
+const uri =  "mongodb+srv://aishatadekunle877:aishat@cluster0.t92x8pf.mongodb.net/Marchcohort?retryWrites=true&w=majority&appName=Cluster0"
+
+
+const connect = async() =>{
+  try {
+    const connected = await mongoose.connect(uri)
+    if (connected) {
+      console.log("connection to database is successful");
+      
+    }
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+connect()
 
 
 const port = 5006
